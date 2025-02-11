121b12e83ae7f518657fb1727ff1bf184d45eccd
Sunny <gsaipraneeth25@gmail.com>
2025-02-10 23:45:16 -0500

    Update components with api interceptor and include jwt

---
+++ src/components/ui/ProtectedRoutes.jsx
@@ -0,0 +1,28 @@
+// src/ui/ProtectedRoutes.jsx
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+const ProtectedRoutes = () => {
+  // Check if the user is authenticated
+  const isAuthenticated = () => {
+    const accessToken = localStorage.getItem("access");
+    if (!accessToken) return false;
+
+    // Decode the token to check its expiry
+    try {
+      const decoded = jwtDecode(accessToken);
+      const expiryDate = decoded.exp;
+      const currentTime = Date.now() / 1000;
+      return expiryDate > currentTime; // Return true if the token is not expired
+    } catch (error) {
+      console.error("Error decoding token:", error);
+      return false;
+    }
+  };
+
+  // If the user is authenticated, render the child routes
+  // Otherwise, redirect to the login page
+  return isAuthenticated() ? <Outlet /> : <Navigate to="/login" replace />;
+};
+
+export default ProtectedRoutes;
